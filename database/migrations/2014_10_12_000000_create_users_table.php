ea87ec5348427949e63b42fbb29c6cee90aae0c7
aaronmusa <jbmusa@htechcorp.net>
2017-10-10 15:44:28 +0800

    doctrine/dbal dependency added

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,12 +13,18 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
+
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('username');
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+        });
+
+        Schema::table('users', function($table)
+        {
+            $table->renameColumn('name', 'username');
         });
 
         DB::table('users')->insert(
